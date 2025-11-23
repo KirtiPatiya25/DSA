e13ab867f7c6fec7faf4f4e224674eedde15c700
Kirti Patiya <Kirtipatiya897@gmail.com>
2025-11-23 22:41:10 +0530

    Time: 1 ms (71.51%) | Memory: 42.4 MB (21.68%) - LeetSync

---
+++ 202-happy-number/happy-number.java
@@ -0,0 +1,30 @@
+class Solution {
+    public int sumOfSquares(int n){
+        int sum = 0;
+        while (n>0){
+            int dig = n % 10 ;
+            sum = sum + (dig * dig);
+            n = n / 10;
+        }
+        return sum;
+    }
+    public boolean isHappy(int n) {
+        int slow = n;
+        int fast = n;
+
+        while(fast != 1){
+            slow = sumOfSquares(slow);
+            fast = sumOfSquares(sumOfSquares(fast));
+
+            if(fast == 1){
+                return true;
+            }
+
+            if(slow == fast){
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
