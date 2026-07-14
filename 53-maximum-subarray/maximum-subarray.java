8e03d980748fa5d079a5188c265718f63fbb2cf3
Kirti Patiya <Kirtipatiya897@gmail.com>
2026-07-15 00:46:21 +0530

    Time: 1 ms (99.97%) | Memory: 77.5 MB (14.16%) - LeetSync

--- 53-maximum-subarray/maximum-subarray.java
+++ 53-maximum-subarray/maximum-subarray.java
@@ -3,11 +3,12 @@ class Solution {
         int currSum = nums[0];
         int maxSum = nums[0];
 
-        for(int i = 1; i<nums.length; i++){
+        for(int i=1; i<nums.length; i++){
             currSum = Math.max(nums[i], currSum + nums[i]);
 
             maxSum = Math.max(currSum, maxSum);
         }
         return maxSum;
+        
     }
 }
