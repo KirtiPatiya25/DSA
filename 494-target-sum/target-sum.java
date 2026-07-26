3c1709166ee23c4fae1103920e7cecc1132e51d8
Kirti Patiya <Kirtipatiya897@gmail.com>
2026-07-27 00:34:09 +0530

    Time: 627 ms (19.54%) | Memory: 42.8 MB (97.62%) - LeetSync

--- 494-target-sum/target-sum.java
+++ 494-target-sum/target-sum.java
@@ -1,22 +1,49 @@
 class Solution {
-    static int sum;
-    public int ways(int i, int[] arr, int res, int target, int[][] dp) {
-        if(i==arr.length){
-            if(res==target) return 1;
-            else return 0;
+    private int solve(int[] nums, int index, int sum, int target){
+        if(index == nums.length){
+            return sum == target ? 1 : 0;
         }
-        if(dp[i][res+sum] != -1) return dp[i][res+sum];
-        int add = ways(i+1, arr, res+arr[i], target, dp);
-        int sub = ways(i+1, arr, res-arr[i], target, dp);
-        return dp[i][res+sum] = add+sub;
+        int plus = solve(nums, index+1, sum+nums[index], target);
+        int minus = solve(nums, index+1, sum-nums[index], target);
+
+        return plus + minus;
     }
-    public int findTargetSumWays(int[] arr, int target) {
-        sum = 0;
-        int n = arr.length;
-        for(int ele: arr) sum += ele;
-        int[][] dp = new int[n][2*sum+1];
-        for(int i=0; i<dp.length; i++)
-            for(int j=0; j<dp[0].length; j++) dp[i][j] = -1;
-        return ways(0, arr, 0, target, dp);
+    public int findTargetSumWays(int[] nums, int target) {
+        return solve(nums, 0, 0, target);
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+//class Solution {
+//     static int sum;
+//     public int ways(int i, int[] arr, int res, int target, int[][] dp) {
+//         if(i==arr.length){
+//             if(res==target) return 1;
+//             else return 0;
+//         }
+//         if(dp[i][res+sum] != -1) return dp[i][res+sum];
+//         int add = ways(i+1, arr, res+arr[i], target, dp);
+//         int sub = ways(i+1, arr, res-arr[i], target, dp);
+//         return dp[i][res+sum] = add+sub;
+//     }
+//     public int findTargetSumWays(int[] arr, int target) {
+//         sum = 0;
+//         int n = arr.length;
+//         for(int ele: arr) sum += ele;
+//         int[][] dp = new int[n][2*sum+1];
+//         for(int i=0; i<dp.length; i++)
+//             for(int j=0; j<dp[0].length; j++) dp[i][j] = -1;
+//         return ways(0, arr, 0, target, dp);
+//     }
+// }
