8a2cf3e99c6aa294715752b2fa0cb384d24f4f50
Kirti Patiya <Kirtipatiya897@gmail.com>
2026-09-08 00:44:14 +0530

    Time: 4 ms (37.40%) | Memory: 268.9 MB (8.15%) - LeetSync

--- 189-rotate-array/rotate-array.java
+++ 189-rotate-array/rotate-array.java
@@ -1,15 +1,20 @@
 class Solution {
-  public void rotate(int[] nums, int k) {
-    if (nums == null || nums.length == 0) {
-            return;
-        }
+    public void rotate(int[] nums, int k) {
+        int n = nums.length;
+        k = k % n;
+        reverse(nums, 0, n-1);
+        reverse(nums, 0, k-1);
+        reverse(nums, k, n-1);
 
-        int[] a = new int[nums.length];
-        for (int i = 0; i < nums.length; i++) {
-            a[(i + k) % nums.length] = nums[i];
+    }
+    private void reverse(int[] nums, int left, int right){
+        while(left<right){
+            int temp = nums[left];
+            nums[left] = nums[right];
+            nums[right] = temp;
+
+            left++;
+            right--;
         }
-        for (int i = 0; i < nums.length; i++) {
-            nums[i] = a[i];
-        }
-  }
+    }
 }
